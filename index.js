5e0956afa02af8ab46aed40838daacba4db5121b
Kantita01 <kantita5069@gmail.com>
2024-01-04 15:21:17 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// import from declrations
+// import { sayHello, sayBye } from "./module6.js";
+// import with alias
+import * as say from "./module6.js";
+say.sayHello("Jhon")
+say.sayBye("Good bye")
+
+//import
+import hello from "./module5.js"
+hello();
+
+// import default
+import greet from "./module4.js"
+const fname = greet('Bill Gates')
+console.log(fname)
+
+//import muliple
+import { name, sum } from "./module3.js";
+console.log(name)
+
+console.log(sum(25,10)) //35
+
+
+// import the function
+import { person } from "./module2.js";
+const display = person("Mark Zuckerberg");
+console.log(display)
+
+// import named
+import { department, task, students } from "./module1.js";
+console.log(department)
+console.log(task[0], task[1], task[2])
+console.log(students.id,students.name,students.age)
---
+++ module6.js
@@ -0,0 +1,10 @@
+// export from declrations
+function sayHello(user) {
+    alert(`Hello,${user}!`)
+}
+
+function sayBye(user) {
+    alert(`Bye, ${user}!`)
+}
+
+export { sayHello, sayBye }
